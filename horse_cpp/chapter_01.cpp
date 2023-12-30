258d612804eaa6b8653ba63fb29583f50292cfbc
vickllny <vickllny@163.com>
2023-12-30 22:51:59 +0800

    0基础cpp

---
+++ horse_cpp/chapter_01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+// https://www.bilibili.com/video/BV1et411b73Z?p=2&vd_source=240260c8cdca141b2447105a672fe65e
+
+#define month_of_year 7
+
+int main(){
+    cout << "this is horst" << endl;
+    int a = 10;
+    cout << "the value of a is: " << a <<endl;
+    const int day_of_week = 7;
+    cout << "month of year:" << month_of_year << endl;
+    cout << "day of week:" << day_of_week << endl;
+    return 0;
+}
---
+++ horse_cpp/chapter_02.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+#define url "https://www.bilibili.com/video/BV1et411b73Z/?p=8&spm_id_from=pageDriver&vd_source=240260c8cdca141b2447105a672fe65e"
+
+int main(){
+    cout << "url is: " << url << endl;
+    return 0;
+}
