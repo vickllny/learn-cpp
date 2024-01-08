82cd2587140a19d8e5ff101d2df7fbf90793a0d7
vickllny <vickllny@163.com>
2024-01-08 22:21:07 +0800

    1.4 f

--- primer/chapter_01_1-4-4.cpp
+++ primer/chapter_01_1-4-4.cpp
@@ -6,5 +6,21 @@ using namespace std;
 //page 15
 
 int main(){
+    //1.17 1.18
+    int currentVal = 0, val = 0;
+    cout << "Please type some numbers." << endl;
+    if(cin >> currentVal){
+        int cnt = 1;
+        while(cin >> val){
+            if(val == currentVal){
+                ++cnt;
+            }else {
+                cout << "The value " << currentVal << " occurs " << cnt << " times." << endl;
+                currentVal = val;
+                cnt = 1;
+            }
+        }
+        cout << "The value " << currentVal << " occurs " << cnt << " times." << endl;
+    }
     return 0;
 }
