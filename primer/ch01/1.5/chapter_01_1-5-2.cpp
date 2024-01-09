40772336c40b0f03462b556a733df9581be82b3b
vickllny <vickllny@163.com>
2024-01-09 22:33:44 +0800

    finished chapter01

---
+++ primer/ch01/1.5/chapter_01_1-5-2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <map>
+#include "Sales_item.h"
+
+using namespace std;
+
+//1.5.2
+//page 21
+
+int main(){
+    //1.23
+    // map<string, int> map;
+    // Sales_item item;
+    // while(cin >> item){
+    //     auto it = map.find(item.isbn());
+    //     if(it == map.end()){
+    //         map.insert(pair<string, int>(item.isbn(), item.sold()));
+    //     }else {
+    //         it->second = it->second + item.sold();
+    //     }
+    // }
+
+    // for(auto it = map.begin(); it != map.end(); it++){
+    //     cout << "isbn => " << it->first << " , count => " << it->second << endl;
+    // }
+
+    //1.24 TODO
+    Sales_item total;
+    if(cin >> total){
+        Sales_item temp;
+        while(cin >> temp){
+            if(total.isbn() == temp.isbn()){
+                total += temp;
+            }else {
+                cout << total << endl;
+                total = temp;
+            }
+        }
+        cout << total << endl;//打印最后一本书
+    }else {
+        cerr << "No data!?" << endl;
+        return -1;
+    }
+    return 0;
+}
