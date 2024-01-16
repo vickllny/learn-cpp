f5c066ab65e0866d199cd211b88d638cfbedeccb
vickllny <vickllny@163.com>
2024-01-17 07:01:41 +0800

    TODO 2.2.4

--- primer/ch02/chapter_02-2-2-3.cpp
+++ primer/ch02/chapter_02-2-2-3.cpp
@@ -7,7 +7,12 @@ using namespace std;
 //标识符
 
 int main(){
-    //TODO
+    //2.2.3
+    // int double = 3.14; illegal
+    // int _ = 2; ok
+    // int catch-22; illegal
+    // int 1_or_2 = 2;//illegal
+    // double Double = 3.14; ok
 
     return 0;
 }
---
+++ primer/ch02/chapter_02-2-2-4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+//2.2.4
+//page 43
+//作用域
+
+int main(){
+    //TODO
+    return 0;
+}
