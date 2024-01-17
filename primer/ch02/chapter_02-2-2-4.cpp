437a940f88db89a4c7cd93a81c667f5a7bbc4be5
vickllny <vickllny@163.com>
2024-01-17 12:04:18 +0800

    2.3.1 TODO

--- primer/ch02/chapter_02-2-2-4.cpp
+++ primer/ch02/chapter_02-2-2-4.cpp
@@ -6,7 +6,28 @@ using namespace std;
 //page 43
 //作用域
 
+int reuse = 42;
+
+int i = 42;
 int main(){
-    //TODO
+    // TODO
+    int unique = 0;
+    cout << reuse << " " << unique << endl;
+    int reuse = 0;
+    cout << reuse << " " << unique << endl;
+
+    cout << ::reuse << " " << unique << endl;
+
+    // 2.13
+    // int i = 100;
+    // int j = i; //j => 100
+    // cout << j << endl;
+
+    // 2.14
+    int i = 100, sum = 0;
+    for(int i = 0; i != 10; ++i){
+        sum += i;
+    }
+    cout << i << " " << sum << endl;
     return 0;
 }
---
+++ primer/ch02/chapter_02-2-3-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+//2.3.1
+//page 45
+//复合类型
+
+int reuse = 42;
+
+int main(){
+
+    return 0;
+}
