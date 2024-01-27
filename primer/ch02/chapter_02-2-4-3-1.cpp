456367aaae65fd5899cadc956bca70efebe190ca
vickllny <vickllny@163.com>
2024-01-27 21:32:58 +0800

    const复习

--- primer/ch02/chapter_02-2-4-3-1.cpp
+++ primer/ch02/chapter_02-2-4-3-1.cpp
@@ -29,5 +29,42 @@ int main(){
     test.vec = { 1, 2, 3, 4, 5};
     test.Print(0);
 
+    // const int a = 20;
+    // int b = a;
+    // const int const *p = &a;
+    // const int *p1 = &a;
+    // const int &r = a;
+    // r = 22;
+    // *p = 22;
+    // *p1 = 22;
+
+    int a = 22;
+    const int a1 = a;
+    int *const a1p = &a1;
+    int a2 = a;
+    const int &ar = a;
+    const int *ap = &a;
+    const int *const ap1 = &a;
+    int *const ap2 = &a;
+    int b = 10;
+    ap = &b;
+    ap1 = &b; // error
+    ap2 = &b;
+
+    int *const ap3 = &a;
+    const int *const ap4 = &ar;
+    *ap3 = 23;
+    *ap4 = 23;
+
+    int &r = 2;
+
+    const int &r1 = 2;
+    const int *p1 = &a1;
+
+    const int const & r2;
+
+    int *const cp;
+    const int *cp1;
+
     return 0;
 }
