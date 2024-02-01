ac290ce9bc815f038d28a94faac12c99798035c3
vickllny <vickllny@163.com>
2024-02-01 21:47:53 +0800

    3.3.2 finish

--- primer/ch03/chapter_03-3-2.cpp
+++ primer/ch03/chapter_03-3-2.cpp
@@ -8,5 +8,23 @@ using namespace std;
 
 int main(){
 
+    // 3.14
+    vector<int> v314;
+    int temp;
+    while(cin >> temp){
+        v314.push_back(temp);
+    }
+
+    for(auto i : v314){
+        cout << i << endl;
+    }
+
+    // 3.15
+    vector<string> s315;
+    string s_temp;
+    while(cin >> s_temp){
+        s315.push_back(s_temp);
+    }
+
     return 0;
 }
