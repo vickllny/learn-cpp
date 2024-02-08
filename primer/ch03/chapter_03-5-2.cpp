d2b7629f5320d17256d07198108ee7a6f3489b76
vickllny <vickllny@163.com>
2024-02-08 23:50:19 +0800

    TODO 3.5.3

--- primer/ch03/chapter_03-5-2.cpp
+++ primer/ch03/chapter_03-5-2.cpp
@@ -8,5 +8,32 @@ using namespace std;
 
 int main(){
 
+    // 3.30
+    constexpr size_t array_size = 10;
+    int ia[array_size];
+    for(size_t ix = 1; ix <= array_size; ++ix){ //ix <= array_size 数据越界；ix的初始值应该是0
+        ia[ix] = ix;
+    }
+
+    // 3.31
+    for(size_t ix = 0; ix < array_size; ++ix){ 
+        ia[ix] = ix;
+    }
+
+    // 3.32
+    int ia1[array_size];
+    for(size_t ix = 0; ix < array_size; ++ix){ 
+        ia1[ix] = ia[ix];
+    }
+    vector<int> iv;
+    for(size_t ix = 0; ix < array_size; ++ix){ 
+        iv.push_back(ix);
+    }
+    vector<int> iv1 = iv;
+
+    // 3.33
+    // 如果scores未初始化，则数组的元素是未定义的，使用++运算符将出现错误
+
+
     return 0;
 }
---
+++ primer/ch03/chapter_03-5-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+// 3.5.3 指针和数组
+// page 105
+
+
+int main(){
+
+    return 0;
+}
